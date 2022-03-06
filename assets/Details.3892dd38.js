372b1a0120df885c9cbdc04f4a83e93d45d4df11
QiHua Huo <huoqihua@cn.ibm.com>
2022-03-07 02:23:10 +0800

    build

---
+++ assets/Details.3892dd38.js
@@ -0,0 +1,1 @@
+import{g as t,u as a,o as c,b as l,e}from"./vendor.b6e8a8be.js";import"./index.9c675efc.js";const p={setup(r){t();const o=a(),s=()=>{console.log(111),o.replace({name:"Home"})};return(n,u)=>(c(),l("section",null,[e("main",null,[e("ul",{class:"color-card"},[e("li",{onClick:s},"wwwx")])])]))}};export{p as default};
